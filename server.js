793fcbb55ee080e342db71a506d9e2616212b944
Kayla Wood <kaylaewood@gmail.com>
2020-02-01 13:02:10 -0700

    Refactor error throwing

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+const throwError = (response, statusCode, message) => {
+  return response.status(statusCode).send({ error: message });
+}
+
 const checkSection = (response, section) => {
   if (section !== 'categories' && section !== 'campaigns') {
-    return response
-    .status(422)
-    .send({ error: `The section ${section} does not exist. The only sections are categories and campaigns.` });
+    throwError(response, 422, `The section ${section} does not exist. The only sections are categories and campaigns.`);
   }
-}
+};
 
 app.use(express.json());
 
@@ -30,7 +32,7 @@ app.get('/api/v1/:section', async (request, response) => {
     const data = await database(`${section}`).select();
     response.status(200).json(data);
   } catch(error) {
-    response.status(500).send({ error: `Internal Server Error: Something went wrong with your request. Please try again.` });
+    throwError(response, 500, 'Internal Server Error: Something went wrong with your request. Please try again.');
   };
 });
 
@@ -44,12 +46,12 @@ app.get('/api/v1/:section/:id', async (request, response) => {
     const item = data.find(item => item.id === parseInt(id));
 
     if (!item) {
-      return response.status(404).send({ error: `There is no item in the ${section} with an id of ${id}.` })
+      throwError(response, 404, `There is no item in the ${section} with an id of ${id}.`);
     };
 
     response.status(200).json(item);
   } catch(error) {
-    response.status(500).send({ error: `Internal Server Error: Something went wrong with your request. Please try again.` });
+    throwError(response, 500, `Internal Server Error: Something went wrong with your request. Please try again.`);
   };
 });
 
@@ -68,9 +70,7 @@ app.post('/api/v1/:section', async (request, response) => {
 
   for (var i = 0; i < requiredParameters.length; i++) {
     if (!newItem[requiredParameters[i]]) {
-      return response
-        .status(422)
-        .send({ error: `Expected format: {${requiredParameters.map(param => ` ${param}: <String>`)} }. You are missing the ${requiredParameters[i]} property.` });
+      throwError(response, 422, `Expected format: {${requiredParameters.map(param => ` ${param}: <String>`)} }. You are missing the ${requiredParameters[i]} property.`);
     };
   };
 
@@ -80,15 +80,13 @@ app.post('/api/v1/:section', async (request, response) => {
     const matchingCategory = categories.find(category => category.id === category_id);
 
     if (!matchingCategory) {
-      return response
-        .status(422)
-        .send({ error: `A category with an id of ${category_id} does not exist. You can only create campaigns in existing categories.` });
+      throwError(response, 422, `A category with an id of ${category_id} does not exist. You can only create campaigns in existing categories.`);
     };
   };
 
   database(`${section}`).insert(newItem, 'id')
     .then(response.status(201).json(newItem))
-    .catch(response.status(500).send({ error: `Internal Server Error: Something went wrong with your request. Please try again.` }));
+    .catch(throwError(response, 500, `Internal Server Error: Something went wrong with your request. Please try again.`));
 });
 
 app.delete('/api/v1/:section', async (request, response) => {
@@ -98,27 +96,23 @@ app.delete('/api/v1/:section', async (request, response) => {
   checkSection(response, section);
 
   if (section === 'categories') {
-    return response
-      .status(422)
-      .send({ error: `You cannot delete a category. You can only delete an individual campaign.` });
+    throwError(response, 422, `You cannot delete a category. You can only delete an individual campaign.`);
   } else if (!id) {
-      return response
-      .status(422)
-      .send({ error: `Expected format: { id: <Number> }. You are missing the id property.` });
+    throwError(response, 422, `Expected format: { id: <Number> }. You are missing the id property.`);
   };
 
   const campaigns = await database(`campaigns`).select();
   const campaign = campaigns.find(campaign => campaign.id === parseInt(id));
 
   if (!campaign) {
-    return response.status(404).send({ error: `There is no campaign with an id of ${id}.` })
+    throwError(response, 404, `There is no campaign with an id of ${id}.`);
   };
 
   database('campaigns')
     .where('id', parseInt(id))
     .del()
     .then(response.status(200).json(id))
-    .catch(response.status(500).send({ error: `Internal Server Error: Something went wrong with your request. Please try again.` }));
+    .catch(throwError(response, 500, `Internal Server Error: Something went wrong with your request. Please try again.`));
 });
 
 app.listen(app.get('port'), () => {
